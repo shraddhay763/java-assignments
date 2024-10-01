6f79a7ec86ee3fc32f1f92c15d60e9bd2a21f086
shraddha yadav <shraddhayadav763@gmail.com>
2024-10-01 21:06:28 +0530

    day 2 code added

---
+++ Day 2/program2.java
@@ -0,0 +1,16 @@
+class whileloop{
+	public static void main(String[]args){
+		int a=1;
+		 while(a<=20){
+			if(a%2==1){
+			  System.out.println("odd numbers are" +a);
+			  a++;
+			}else {
+				System.out.println("even numbers are"+a);
+			a++;
+		   }		
+		}		
+
+	   }
+
+	}
---
+++ Day 2/program5.java
@@ -0,0 +1,10 @@
+class whileloop{
+	public static void main(String[]args){
+	  int a=1;
+	    while(a<=20){
+		if(a%3==0 && a%7==0){
+			System.out.print(a);
+		  }
+		}
+	     }
+	}
