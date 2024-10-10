c8d4b200652ba7fcbf81881433e2c16c207c63f1
shraddha yadav <shraddhayadav763@gmail.com>
2024-10-10 18:23:17 +0530

    day 8 code added

---
+++ Day 8/p4.java
@@ -0,0 +1,22 @@
+class demo{
+	public static void main(String[]args){
+	int a=1;
+	for(int i=1;i<=4;i++){
+	 for(int j=1;j<=i;j++){
+	  if(j%2==1){	
+		System.out.print(" + ");
+	   }else{
+		System.out.print(" = ");
+	     }
+	   }
+       		System.out.println();
+	}
+     }
+  }
+
+Output:-
+ +
+ +  =
+ +  =  +
+ +  =  +  =
+
